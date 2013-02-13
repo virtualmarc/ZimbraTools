4b9a067fae241d84c2df60c1407f09f42f077366
virtualmarc <admin@vmtek.de>
2013-02-13 00:24:34 -0800

    Create email.sh

---
+++ import/email.sh
@@ -0,0 +1,54 @@
+#!/bin/zsh
+# Import Script
+# Import .eml E-Mail files
+# Dont forget to install zsh and set the ZCSACCOUNT
+# (c) 2013 by virtualmarc @ GitHub
+# Thanks to: lukas2511 @ GitHub
+
+ZCSACCOUNT="user@domain.tld"
+
+function newName()
+{
+        content=${@// /}
+        echo -n ${content//[^a-zA-Z0-9\.\-_]/}
+}
+
+echo "Beginning Import"
+IFS=$'\n'
+CURDIR='/'
+for folder in $(find . -type d)
+do
+        if [ "$folder" != "." ]
+        then
+                NEWFOLDER=`newName $folder`
+                echo "Current Folder: $folder"
+                if [ "$folder" != $NEWFOLDER ]
+                then
+                        echo "New name: $NEWFOLDER"
+                        mv "$folder" $NEWFOLDER
+                fi
+                echo "Creating Folder: $NEWFOLDER"
+                ZCSPATH=${NEWFOLDER#"."}
+                echo createFolder $ZCSPATH | zmmailbox -z -m $ZCSACCOUNT
+                for file in $(find $NEWFOLDER)
+                do
+                        FILENAME=${file#"$NEWFOLDER/"}
+                        NEWFILE=`newName $FILENAME`
+                        echo "Current File: $NEWFOLDER/$FILENAME"
+                        if [ "$FILENAME" != $NEWFILE ]
+                        then
+                                echo "New name: $NEWFILE"
+                                mv "$NEWFOLDER/$FILENAME" "$NEWFOLDER/$NEWFILE"
+                        fi
+                        case $NEWFOLDER/$NEWFILE in
+                                *.eml)
+                                        echo "Found E-Mail: $NEWFOLDER/$NEWFILE"
+                                        echo addMessage $ZCSPATH $NEWFOLDER/$NEWFILE | zmmailbox -z -m $ZCSACCOUNT
+                                ;;
+                                *)
+                                        echo "Found Unknown file: $NEWFOLDER/$NEWFILE"
+                                ;;
+                        esac
+                done
+        fi
+done
